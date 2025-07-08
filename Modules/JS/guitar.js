e52c8d47d6abeceb7ece99bce90ed08a047664e9
Radben97 <radben.97@gmail.com>
2025-07-09 01:58:56 +0530

    Delete Modules/JS directory

--- Modules/JS/guitar.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const playing = () => { return "Playing guitar yo!!" };
-const shredding = () => { return "shredding the dick yooo!!" };
-const plucking = () => { return "Plucking the strings yoooooo!!" };
-export default playing;
-export { shredding, plucking };
--- Modules/JS/user.js
+++
@@ -1,9 +0,0 @@
-class user {
-    constructor(email, name) {
-        this.id = email,
-        this.name = name
-    }
-    display() { return `greetings, my name is ${this.name}` };
-}
-
-export default user;
